0db7eb80b887373ccca1c58e92c84769f5f7ee44
pastebt <pastebt@github.com>
2018-08-09 17:02:13 -0700

    can sort

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ clean:
 	@rm -f goasm
 	@
 
-goasm: goasm.go
+goasm: *.go
 	@#GOROOT="/home/yma/workspace/golang/go1.9.4"; PATH="/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/home/yma/bin:/home/yma/workspace/golang/go1.9.4/bin"; go version; go build fure_hide.go
 	@GOROOT="/home/yma/workspace/golang/go1.11beta3"; PATH="/home/yma/workspace/golang/go1.11beta3/bin"; go version; GOOS=js GOARCH=wasm go build goasm.go table.go
 	@
--- table.go
+++ table.go
@@ -1,25 +1,43 @@
 package main
 
 import (
+    "sort"
     "syscall/js"
     "fgdwcfgo/log"
 )
 
 
-type Row []js.Value
+type Row struct {
+    tr  js.Value
+    tds []js.Value
+}
 
 
 type Table struct {
     id   string
     elm  js.Value       // table js element
+    body js.Value       // table body element, use to redraw table
     cols []js.Value     // table headers list
     rows []Row          // table body tr list
+
 }
 
+// c: column
+// t: type
+// o: order
+func less(rows []Row, col, typ, ord int) func(i, j int) bool {
+    return func(i, j int) bool {
+    I := rows[i].tds[col].Get("innerText")
+    J := rows[j].tds[col].Get("innerText")
+    // TODO, typ and ord
+    return I.String() < J.String()
+}
+}
 
 func NewTable(id string) *Table {
     return &Table{id: id,
                   cols: make([]js.Value, 0, 5),
+                  rows: make([]Row, 0, 10),
                   }
 }
 
@@ -39,7 +57,17 @@ func (t *Table)Init() {
 // return func will copy col_id to local, which is necessory
 func (t *Table)do_sort(col_id int) func([]js.Value) {
     return func([]js.Value) {
+    sort.Slice(t.rows, less(t.rows, col_id, 0, 0))
     log.Debugf("do_sort table %s, column %d", t.id, col_id)
+    for _, row := range t.rows {
+        for _, td := range row.tds {
+            s := td.Get("innerText").String()
+            log.Debugf("s = %#v", s)
+        }
+        t.body.Call("removeChild", row.tr)
+        t.body.Call("appendChild", row.tr)
+    }
+
     }
 }
 
@@ -61,10 +89,24 @@ func (t *Table)get_head() {
 
 
 func (t *Table)get_body() {
-    thd := t.elm.Call("getElementsByTagName", "tbody")
-    if thd.Length() < 1 {
+    tbd := t.elm.Call("getElementsByTagName", "tbody")
+    if tbd.Length() < 1 {
         log.Errorf("Can not find tbody of table %s", t.id)
         return
     }
+    t.body = tbd.Index(0)
+    trs := t.body.Call("getElementsByTagName", "tr")
+    for i := 0; i < trs.Length(); i++ {
+        t.rows = append(t.rows, t.get_row(trs.Index(i)))
+    }
+}
 
+
+func (t *Table)get_row(tr js.Value) Row {
+    row := Row{tr: tr, tds: make([]js.Value, 0, 5)}
+    tds := tr.Call("getElementsByTagName", "td")
+    for i := 0; i < tds.Length(); i++ {
+        row.tds = append(row.tds, tds.Index(i))
+    }
+    return row
 }
