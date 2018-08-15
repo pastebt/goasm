5e3bd1643149ead79379a7f2a6028cfb2592ed8c
pastebt <pastebt@github.com>
2018-08-15 16:26:47 -0700

    gen date div

--- date.go
+++ date.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+    "fmt"
+    "time"
     "syscall/js"
 
     "fgdwcfgo/log"
@@ -12,6 +14,11 @@ type DatePicker struct {
     elm  js.Value       // input js element
     btn  js.Value       // generated button
 }
+
+var (
+    DpDiv js.Value
+    Tbody js.Value
+)
 
 
 func initPickDate() {
@@ -30,6 +37,8 @@ func initPickDate() {
                 #background-position: center left;
             }
             `)
+    //hd := doc.Get("head")
+    //if hd.Type() == js.TypeNull {
     hd := doc.Call("getElementsByTagName", "head")
     if hd.Length() < 1 {
         log.Errorf("Can not find <head> in html ")
@@ -37,6 +46,42 @@ func initPickDate() {
     }
     hd.Index(0).Call("insertBefore", st, hd.Index(0).Get("firstChild"))
 
+    DpDiv := doc.Call("createElement", "div")
+    DpDiv.Get("classList").Call("add", "datepicker")
+    DpDiv.Set("innerHTML", `
+        <table class="year">
+            <tr><td>lf</td><td>Mon</td><td>Yea</td><td>rt</td></tr>
+        </table>
+        <table class="week">
+            <thead><tr>
+                <th>Su</th><th>Mo</th><th>Tu</th><th>We</th><th>Th</th><th>Fr</th><th>Sa</th>
+            </tr></thead>
+            <tbody>
+            <tr><td>1</td><td>6</td><td>8</td><td>0</td><td>1</td><td>2</td><td>2</td></tr>
+            <tr><td>2</td><td>3</td><td>5</td><td>8</td><td>0</td><td>1</td><td>2</td></tr>
+            <tr><td>2</td><td>4</td><td>6</td><td>8</td><td>9</td><td>1</td><td>2</td></tr>
+            <tr><td>2</td><td>4</td><td>5</td><td>8</td><td>0</td><td>1</td><td>2</td></tr>
+            <tr><td>2</td><td>4</td><td>6</td><td>7</td><td>9</td><td>1</td><td>3</td></tr>
+            </tbody>
+        </table>
+           `)
+    tbs := DpDiv.Call("getElementsByTagName", "tbody")
+    for i := 0; i < tbs.Length(); i++ {
+        trs := tbs.Index(i).Call("getElementsByTagName", "tr")
+        if trs.Length() == 5 {
+            Tbody = tbs.Index(i)
+            break
+        }
+    }
+    //bd := doc.Get("body") // dom3 not support yet
+    //if bd.Type() == js.TypeNull {
+    bd := doc.Call("getElementsByTagName", "body")
+    if bd.Length() < 1 {
+        log.Errorf("Can not find <body> in html ")
+        return
+    }
+    bd.Index(0).Call("appendChild", DpDiv)
+    update_table(time.Now())
 }
 
 
@@ -60,3 +105,27 @@ func (d *DatePicker)Init() {
                                  d.btn, d.elm.Get("nextSibling"))
 }
 
+
+func update_table(n time.Time) {
+    //log.Debugf("n = %v", n)
+    dt := n.AddDate(0, 0, 1 - n.Day())   // 1st day of month
+    //log.Debugf("dt = %v", dt)
+    dt = dt.AddDate(0, 0, -int(dt.Weekday()))   // Sunday
+    //log.Debugf("dt = %v", dt)
+    trs := Tbody.Call("getElementsByTagName", "tr")
+    for r := 0; r < 5; r++ {
+        tds := trs.Index(r).Call("getElementsByTagName", "td")
+        for c := 0; c < 7; c++ {
+            //days[r][c] = d
+            cs := tds.Index(c).Get("classList")
+            if dt.Month() == n.Month() {
+                cs.Call("remove", "other")
+            } else {
+                cs.Call("add", "other")
+            }
+            h := fmt.Sprintf(`<a href="#">%d</a>`, dt.Day())
+            tds.Index(c).Set("innerHTML", h)
+            dt = dt.AddDate(0, 0, 1)
+        }
+    }
+}
